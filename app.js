8c96e1bae959a8ba06022f020dc16c5da339bccf
Lane <lanericharddavis@gmail.com>
2021-03-26 20:48:31 -0600

    dynamic count

--- app.js
+++ app.js
@@ -1,42 +1,108 @@
-let fishCount = 0
+// let fishClickUpgrades = [
+//   {
+//     name: "pole",
+//     value: 2,
+//     currentValue: 0
+//   },
+//   {
+//     name: "net",
+//     value: 10,
+//     currentValue: 0
+//   },
+//   {
+//     name: "boat",
+//     value: 50,
+//     currentValue: 0
+//   },
+//   {
+//     name: "fleet",
+//     value: 200,
+//     currentValue: 0
+//   }
+// ]
 
-let fishClickUpgrades = {
-  pole: 0,
-  net: 0,
-  boat: 0,
-  fleet: 0,
+
+
+let poleUpgrades = {
+  'value': 2,
+  'currentValue': 0
 }
+let netUpgrades = {
+  'value': 10,
+  'currentValue': 0
+}
+let boatUpgrades = {
+  'value': 50,
+  'currentValue': 0
+}
+let fleetUpgrades = {
+  'value': 200,
+  'currentValue': 0
+}
+let autoUpgrades = {}
+let fishCount = 0
+let clickValue = 1
+let poleCount = 0
+let netCount = 0
+let boatCount = 0
+let fleetCount = 0
+
 
 
 function goFish() {
   document.getElementById("fishImg")
-  fishCount++
+  fishCount += clickValue
   console.log("fish")
   draw()
 }
 
-function poleMultiplier() {
-  document.getElementById("poleMultipier")
-  fishClickUpgrades.pole++
-  console.log("pole")
+function drawStats() {
+  poleCount = poleUpgrades.currentValue
+  netCount = netUpgrades.currentValue
+  boatCount = boatUpgrades.currentValue
+  fleetCount = fleetUpgrades.currentValue
 }
 
-function netMultiplier() {
-  document.getElementById("netMultipier")
-  fishClickUpgrades.net++
-  console.log("net")
+function poleCounter() {
+  if (fishCount >= 10) {
+    document.getElementById("poleCounter").disabled = false
+    poleCount++
+    fishCount -= 50
+    console.log("pole")
+    draw()
+  }
+  document.getElementById("poleCounter").disabled = true
 }
 
-function boatMultiplier() {
-  document.getElementById("boatMultipier")
-  fishClickUpgrades.boat++
-  console.log("boat")
+function netCounter() {
+  if (fishCount >= 20) {
+    document.getElementById("netCounter").disabled = false
+    netCount++
+    fishCount -= 200
+    console.log('net')
+    draw()
+  }
+  document.getElementById("netCounter").disabled = true
 }
 
-function fleetMultiplier() {
-  document.getElementById("fleetMultipier")
-  fishClickUpgrades.fleet++
-  console.log("fleet")
+function boatCounter() {
+  if (fishCount >= 30) {
+    document.getElementById("boatCounter").disabled = false
+    boatCount++
+    console.log("boat")
+    draw()
+  }
+  document.getElementById("boatCounter").disabled = true
+}
+
+function fleetCounter() {
+  if (fishCount >= 40) {
+    document.getElementById("fleetCounter").disabled = false
+    fleetCount++
+    console.log("fleet")
+    draw()
+  }
+  document.getElementById("fleetCounter").disabled = true
 }
 
 
@@ -47,10 +113,10 @@ function draw() {
     <div id="fishStats" class="row align-items-center py-5 text-center">
       <div class="col-md-4">
         <p>STATS</p>
-        <p>Fishing Poles: ${fishClickUpgrades.pole}</p>
-        <p>Fishing Nets: ${fishClickUpgrades.net}</p>
-        <p>Fishing Boats: ${fishClickUpgrades.boat}</p>
-        <p>Fishing Fleets: ${fishClickUpgrades.fleet}</p>
+        <p>Fishing Poles: ${poleCount} </p>
+        <p>Fishing Nets: ${netCount} </p>
+        <p>Fishing Boats: ${boatCount} </p>
+        <p>Fishing Fleets: ${fleetCount} </p>
       </div>
       <div id="fishImg" class="col-md-4">
         <img src="//placehold.it/200x200" alt="" onclick="goFish()">
